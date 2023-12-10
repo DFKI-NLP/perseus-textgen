4477bf03e858b292b66efe1e004bdce7c59f0ec1
nid989 <nidbhavsar989@gmail.com>
2023-12-10 16:49:27 +0100

    added `user` and `assistant` functionalities

--- frontend/app.py
+++ frontend/app.py
@@ -12,7 +12,11 @@ from langchain.llms import HuggingFaceTextGenInference
 from langchain.chains import ConversationChain
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 
-DEFAULT_API_ENDPOINT = "http://serv-3337.kl.dfki.de:5000"
+# Prerequisites:
+# - start a backend server,
+#   e.g. with any of the scripts in https://github.com/DFKI-NLP/perseus-textgen/blob/master/scripts
+
+DEFAULT_API_ENDPOINT = "http://serv-3316.kl.dfki.de:5000"
 DEFAULT_PARAMS = {
     "streaming": False,
     "do_sample": False,
@@ -47,10 +51,11 @@ def bot(
     template_str: str,
     system_prior: str,
     log_str: str,
+    user_prefix_str: str,
+    ai_prefix_str: str
 ) -> Iterator[List[List[str]]]:
 
     log = json.loads(log_str)
-
     parameters = json.loads(parameter_str)
     llm = HuggingFaceTextGenInference(
         inference_server_url=endpoint,
@@ -62,7 +67,7 @@ def bot(
         template=template_str,
         template_format="jinja2"
     )
-    memory = ConversationBufferMemory()
+    memory = ConversationBufferMemory(human_prefix=user_prefix_str, ai_prefix=ai_prefix_str)
     for utterance_pair in history[:-1]:
         memory.save_context(
             inputs={"input": utterance_pair[0]},
@@ -74,29 +79,35 @@ def bot(
         verbose=True,
         memory=memory,
     )
-
     latest_user_message = history[-1][0].strip()
     log.append({"request": {"endpoint": endpoint, "prompt": latest_user_message, "history": memory.buffer, "details": True, **parameters}})
     history[-1][1] = ""
     log[-1]["response"] = ""
-    if parameters.get("streaming", False):
-        for response in llm_chain.predict(input=latest_user_message, callbacks=[StreamingStdOutCallbackHandler()]):
-            history[-1][1] += response
+    try:
+        if parameters.get("streaming", False):
+            for response in llm_chain.predict(input=latest_user_message, callbacks=[StreamingStdOutCallbackHandler()]):
+                history[-1][1] += response
+                log[-1]["response"] += str(response)
+                time.sleep(0.02)
+                yield history, json.dumps(log, indent=2)
+        else:
+            response = llm_chain.predict(input=latest_user_message, callbacks=[StreamingStdOutCallbackHandler()])
+            history[-1][1] = response
             log[-1]["response"] += str(response)
-            time.sleep(0.02)
             yield history, json.dumps(log, indent=2)
-    else:
-        response = llm_chain.predict(input=latest_user_message, callbacks=[StreamingStdOutCallbackHandler()])
-        history[-1][1] = response
-        log[-1]["response"] += str(response)
+    except Exception as e:
+        log[-1]["response"] = str(e)
+        del history[-1]
         yield history, json.dumps(log, indent=2)
 
-def update_template_and_system_prior(template_key, template_str, system_prior, templates):
+def update_template_and_system_prior(template_key, template_str, system_prior, user_prefix_str, ai_prefix_str, templates):
     if template_key is None:
-        return template_str, system_prior
+        return template_str, system_prior, user_prefix_str, ai_prefix_str
     new_template_data = templates[template_key]
     new_template_str = json.dumps(new_template_data["template"], indent=2)
-    return new_template_str, new_template_data["system_prior"]
+    new_user_prefix_str = json.dumps(new_template_data["user_prefix"], indent=2)
+    new_ai_prefix_str = json.dumps(new_template_data["ai_prefix"], indent=2)
+    return new_template_str, new_template_data["system_prior"], new_user_prefix_str, new_ai_prefix_str
 
 def start():
     # load templates from json file
@@ -112,6 +123,8 @@ def start():
         lines=6,
         value=json.dumps(templates[DEFAULT_TEMPLATE]["template"], indent=2),
     )
+    user_prefix_str = gr.Code(label="User Prefix", language="json", lines=1, value=json.dumps(templates[DEFAULT_TEMPLATE]["user_prefix"], indent=2), interactive=True)
+    ai_prefix_str = gr.Code(label="AI Prefix", language="json", lines=1, value=json.dumps(templates[DEFAULT_TEMPLATE]["ai_prefix"], indent=2), interactive=True)
     select_template = gr.Dropdown(
         label="Load Template and System Prior (overwrites existing values)",
         choices=list(templates),
@@ -147,7 +160,7 @@ def start():
                         queue=False
                     ).then(
                         bot,
-                        inputs=[chatbot, endpoint, parameters_str, template_str, system_prior, log_str],
+                        inputs=[chatbot, endpoint, parameters_str, template_str, system_prior, log_str, user_prefix_str, ai_prefix_str],
                         outputs=[chatbot, log_str],
                     )
 
@@ -160,17 +173,20 @@ def start():
                 parameters_str.render()
                 with gr.Group():
                     template_str.render()
+                    with gr.Row():
+                        user_prefix_str.render()
+                        ai_prefix_str.render()
                     system_prior.render()
                     select_template.render()
                     select_template.change(
                         partial(update_template_and_system_prior, templates=templates),
-                        inputs=[select_template, template_str, system_prior],
-                        outputs=[template_str, system_prior],
+                        inputs=[select_template, template_str, system_prior, user_prefix_str, ai_prefix_str],
+                        outputs=[template_str, system_prior, user_prefix_str, ai_prefix_str],
                         queue=False,
                     )
 
     demo.queue()
-    demo.launch(debug=True)
+    demo.launch()
 
 if __name__ == "__main__":
     start()
